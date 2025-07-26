62b8bdebfbf1f03ed803a91584ee597b9b924881
Hiva Golkar <hiva.golkar@gmail.com>
2025-07-27 00:15:39 +0330

    Add files via upload

--- hivapanel/celery.py
+++ hivapanel/celery.py
@@ -1,7 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hivapanel.settings')
-app = Celery('hivapanel')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hivapanel.settings")
+
+app = Celery("hivapanel")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
