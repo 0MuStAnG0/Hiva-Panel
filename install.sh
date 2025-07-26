2cffd120c4730be8cca4fd2343686b2426a7c024
Hiva Golkar <hiva.golkar@gmail.com>
2025-07-26 20:22:27 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-# نصب خودکار Hiva Panel
+
+echo "🔧 نصب Hiva Panel..."
+
+sudo apt update
+sudo apt install -y python3 python3-pip postgresql redis git nginx
+
+echo "✅ نصب پیش‌نیازها انجام شد."
+
+git clone https://github.com/0MuStAnG0/Hiva-Panel.git hivapanel
+cd hivapanel
+
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+echo "✅ راه‌اندازی اولیه انجام شد."
